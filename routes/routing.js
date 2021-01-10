673992a901ca572e044bc2c1a0785d2cf2d03705
mrerror418 <pavlo.nmcv@gmail.com>
2021-01-10 22:00:17 +0200

    change name register

--- routes/routing.js
+++ routes/routing.js
@@ -9,7 +9,7 @@ router.use(bodyParser.json());
 
 
 router.get('/', function(req, res) {
-    fs.readFile('../ttsaasjet/client/index.html', function (err, data) {
+    fs.readFile('../ttSaasJet/client/index.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length});
         res.write(data);
         res.end();
